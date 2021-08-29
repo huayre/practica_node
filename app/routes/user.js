2b069a088ee13a142a0f37f189522781533af0b3
Roy Carlos Huayre Bujaico <rhuayre@tumi-soft.com>
2021-08-28 19:50:57 -0500

    02 add functionality update and delete user

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const controllerUser = require('../controllers/ControllerUser')
 
-router.get('/user',controllerUser.index);
-router.post('/user',controllerUser.store);
+router.get('/user', controllerUser.index);
+router.post('/user', controllerUser.store);
+router.put('/user/:id', controllerUser.update);
+router.delete('/user/:id', controllerUser.delete);
 module.exports = router;
